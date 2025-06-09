021b5fa0de235ba99df4418c363cdc93870e2b0b
jungwoo57 <ghkdwjddn12@gmail.com>
2025-06-09 20:26:12 +0900

    feat : 기본 프로젝트 세팅

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [field: SerializeField] public string name { get; private set; }
+    [field: SerializeField] public string atk { get; private set; }
+    [field: SerializeField] public string hp { get; private set; }
+    
+    
+}
